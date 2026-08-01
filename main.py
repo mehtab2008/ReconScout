444188053b9f3372790a9b179db7221e8b0c6d97
unknown <eo.faryal@gmail.com>
2026-08-01 16:39:26 +0500

    Add HTTP and HTTPS availability checks

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import socket
+import requests
+
+def check_status(protocol, hostname):
+    try:
+        response = requests.get(f"{protocol}://{hostname}", timeout=10)
+        if response.status_code == 200:
+            print(f"Status Code: {response.status_code} - Reason : OK")
+        elif response.status_code == 301:
+            print(f"Status Code: {response.status_code} - Reason : Moved Permanently")
+        elif response.status_code == 403:
+            print(f"Status Code: {response.status_code} - Reason : Forbidden")
+        elif response.status_code == 404:
+            print(f"Status Code: {response.status_code} - Reason : Not Found")
+        return 0
+    except requests.RequestException:
+        return False
 
 print("========================================")
 print("               RECON SCOUT              ")
@@ -8,10 +24,18 @@ hostname = input("Enter the hostname: ").strip()
 
 try:
     ip_addr = socket.gethostbyname(hostname)
-    print("\nResult")
+    print("\nTarget Information")
     print("------------------------------")
     print(f"Hostname : {hostname}")
     print(f"IP Address: {ip_addr}")
 except socket.gaierror:
     print(f"Unable to resolve hostname: {hostname}")
+    exit(1)
 
+print("HTTP check")
+print("------------------------------")
+check_status("http", hostname)
+
+print("HTTPS check")
+print("------------------------------")
+check_status("https", hostname)
