6c72b54153723f44b3a1e22af2408ba88fe6468e
unknown <eo.faryal@gmail.com>
2026-08-03 20:16:30 +0500

    Add robots.txt and sitemap.xml reconnaissance

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ headers = [
     "Content-Length",
     "Location"
 ]
-Security_headers = [
+security_headers = [
     "Strict-Transport-Security",
     "Content-Security-Policy",
     "X-Content-Type-Options",
@@ -42,30 +42,59 @@ except socket.gaierror:
     print(f"Unable to resolve hostname: {hostname}")
     exit(1)
 
-print("")
+print()
 print("HTTP check")
 print("------------------------------")
 response = check_status("http", hostname)
 
-print("")
+print()
 print("HTTPS check")
 print("------------------------------")
 response = check_status("https", hostname)
 
-print("")
+print()
 print("Headers check")
 print("------------------------------")
 for header in headers:
     if response and header in response.headers:
-        print(f"{header}: {response.headers[header]}")
+        print(f"{header:<20}: {response.headers[header]}")
     else:
-        print(f"{header}: Not found")
+        print(f"{header:<20}: Not found")
 
-print("")
+print()
 print("Security Headers check")
 print("------------------------------")
-for header in Security_headers:
+for header in security_headers:
     if response and header in response.headers:
-        print(f"{header}: Present")
+        print(f"{header:<30}: Present")
     else:
-        print(f"{header}: Missing")
+        print(f"{header:<30}: Missing")
+
+
+print()
+print("robots.txt check")
+print("------------------------------")
+robots_response = check_status("http", f"{hostname}/robots.txt")
+if robots_response and robots_response.status_code == 200:
+    print("\nrobots.txt content (first 10 lines):")
+    print("------------------------------")
+    lines = robots_response.text.splitlines()
+    for line in lines[:10]:
+        print(line)
+else:
+    print("robots.txt not found or inaccessible.")
+
+print()
+print("sitemap.xml check")
+print("------------------------------")
+sitemap_response = check_status("http", f"{hostname}/sitemap.xml")
+if sitemap_response and sitemap_response.status_code == 200:
+    print("\nsitemap.xml content (first 10 lines):")
+    print("------------------------------")
+    lines = sitemap_response.text.splitlines()
+    for line in lines[:10]:
+        print(line)
+else:
+    print("sitemap.xml not found or inaccessible.")
+
+
